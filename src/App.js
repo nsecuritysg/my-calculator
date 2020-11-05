31ed985a334bfcfa5252397e8f649dab9e92f90b
nsecuritysg <40561053+nsecuritysg@users.noreply.github.com>
2020-11-05 23:03:26 +0300

    add flex, refactoring

--- src/App.js
+++ src/App.js
@@ -1,79 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import {store} from './store';
 
-function App() {
-  const [value, setValue] = React.useState('0');
+export const App = () => {
+  const btns = store.buttons;
+  const [value, setValue] = useState(0);
+
+  const calculator = btns.map((i) => {
+        switch(i.id) {
+            case 1:
+               return <input className={i.style} type='button' value={value} />
+            case 2:
+                return <input className={i.style} type='button' value={i.value} onClick={ () => { setValue(0) } } />
+            case 18:
+                return <input className={i.style} type='button' value={i.value} onClick={ () => { 
+                  let equal = (fn) => { // eslint-disable-next-line
+                    return new Function('return ' + fn)()
+                  } 
+                  setValue(equal(value))} } />
+            default:
+                return <input className={i.style} type='button' value={i.value} 
+                  onClick={ () => { setValue(value + i.value) } } />
+        }                 
+  })
   return (
-    <div className="calc">
-      <form className="form" name="form">
-        <input className="output" value={value}/>
-      </form>  
-      <table>
-        <tr>
-          <td colspan="3"><input className="button ac btnColor" type="button" value="AC" onClick={()=>{
-            setValue('0')
-          }}/></td>
-          <td><input className="button btnColor" type="button" value="/" onClick={()=>{
-            setValue(value + '/')
-          }}/></td>
-        </tr>
-        <tr>
-          <td><input className="button" type="button" value="7" onClick={()=>{
-            setValue(value + '7')
-          }}/></td>
-          <td><input className="button" type="button" value="8" onClick={()=>{
-            setValue(value + '8')
-          }}/></td>
-          <td><input className="button" type="button" value="9" onClick={()=>{
-            setValue(value + '9')
-          }}/></td>
-          <td><input className="button btnColor" type="button" value="*" onClick={()=>{
-            setValue(value + '*')
-          }}/></td>
-        </tr>
-        <tr>
-          <td><input className="button" type="button" value="4"  onClick={()=>{
-            setValue(value + '4')
-          }}/></td>
-          <td><input className="button" type="button" value="5" onClick={()=>{
-            setValue(value + '5')
-          }}/></td>
-          <td><input className="button" type="button" value="6" onClick={()=>{
-            setValue(value + '6')
-          }}/></td>
-          <td><input className="button btnColor" type="button" value="-" onClick={()=>{
-            setValue(value + '-')
-          }}/></td>
-        </tr>
-        <tr>
-          <td><input className="button" type="button" value="1" onClick={()=>{
-            setValue(value + '1')
-          }}/></td>
-          <td><input className="button" type="button" value="2" onClick={()=>{
-            setValue(value + '2')
-          }}/></td>
-          <td><input className="button" type="button" value="3" onClick={()=>{
-            setValue(value + '3')
-          }}/></td>
-          <td><input className="button btnColor" type="button" value="+" onClick={()=>{
-            setValue(value + '+')
-          }}/></td>
-        </tr>
-        <tr>
-          <td><input className="button" type="button" value="0" onClick={()=>{
-            setValue(value + '0')
-          }}/></td>
-          <td><input className="button" type="button" value="."/></td>
-          <td colspan="2"><input className="button equal" type="button" value="=" onClick={()=>{
-            function evil(fn) {
-              return new Function('return ' + fn)();
-            }
-            setValue(evil(value));
-          }}/></td>
-        </tr>
-      </table>
-    </div>
-  );
+    <>
+      {calculator}
+    </>
+  )
 }
-
-export default App;
---
+++ src/store.js
@@ -0,0 +1,22 @@
+export const store = {
+    buttons: [
+        {id: 1, style: 'output'},
+        {id: 2, style: 'button btnColor ac', value: 'AC'},
+        {id: 3, style: 'button btnColor', value: '/'},
+        {id: 4, style: 'button', value: '7'},
+        {id: 5, style: 'button', value: '8'},
+        {id: 6, style: 'button', value: '9'},
+        {id: 7, style: 'button btnColor', value: '*'},
+        {id: 8, style: 'button', value: '4'},
+        {id: 9, style: 'button', value: '5'},
+        {id: 10, style: 'button', value: '6'},
+        {id: 11, style: 'button btnColor', value: '-'},
+        {id: 12, style: 'button', value: '1'},
+        {id: 13, style: 'button', value: '2'},
+        {id: 14, style: 'button', value: '3'},
+        {id: 15, style: 'button btnColor', value: '+'},
+        {id: 16, style: 'button', value: '0'},
+        {id: 17, style: 'button', value: '.'},
+        {id: 18, style: 'button equal', value: '='}
+    ]
+};
