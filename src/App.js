30ac6e9fcb6e079b940ead75278002b9e2d2f222
nsecuritysg <40561053+nsecuritysg@users.noreply.github.com>
2020-11-05 23:51:00 +0300

    remove a zero

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import './App.css';
 import {store} from './store';
 
 export const App = () => {
-  const btns = store.buttons;
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState('');
 
-  const calculator = btns.map((i) => {
+  const calculator = store.buttons.map((i) => {
+    if (i[0] === '') setValue(0);
         switch(i.id) {
             case 1:
                return <input className={i.style} type='button' value={value} />
             case 2:
-                return <input className={i.style} type='button' value={i.value} onClick={ () => { setValue(0) } } />
+                return <input className={i.style} type='button' value={i.value} onClick={ () => { setValue('') } } />
             case 18:
                 return <input className={i.style} type='button' value={i.value} onClick={ () => { 
                   let equal = (fn) => { // eslint-disable-next-line
                     return new Function('return ' + fn)()
                   } 
-                  setValue(equal(value))} } />
+                  setValue(equal(0 + value))} } />
             default:
                 return <input className={i.style} type='button' value={i.value} 
                   onClick={ () => { setValue(value + i.value) } } />
